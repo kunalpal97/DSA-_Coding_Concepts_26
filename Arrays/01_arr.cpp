3d327620593df7c8872033ea3a99910b8e998439
Kunal Pal <strive007boy@gmail.com>
2026-03-25 01:14:26 +0530

    Initial DSA Structure commit

---
+++ Arrays/01_arr.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(){
+
+    int arr[5] = {12 , 45 , 88  , 99 , 55};
+
+    int max_num = INT_MIN;
+    int min_num = INT_MAX;
+
+    for(int i = 0; i<5; i++){
+
+        if(arr[i] > max_num){
+            max_num = arr[i];
+        }
+        else if(arr[i] < min_num){
+            min_num = arr[i];
+        }
+    }
+
+
+    cout <<"Maximum value in array is : " << max_num << endl;
+
+
+    cout <<"Minimum value in array is : " << min_num << endl;
+
+
+    return 0;
+
+
+
+}
