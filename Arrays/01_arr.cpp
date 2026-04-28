ebfee66f4ed0e6025c1b45394b9e7f888a0f9325
Kunal Pal <strive007boy@gmail.com>
2026-04-28 18:44:51 +0530

    made the code formatted

--- Arrays/01_arr.cpp
+++ Arrays/01_arr.cpp
@@ -1,35 +1,28 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int main()
+{
 
-int main(){
-
-    int arr[5] = {12 , 45 , 88  , 99 , 55};
+    int arr[5] = {99, 45, -3, 12, 6};
 
     int max_num = INT_MIN;
     int min_num = INT_MAX;
 
-    for(int i = 0; i<5; i++){
+    for (int i = 0; i < 5; i++)
+    {
 
-        if(arr[i] > max_num){
+        if (arr[i] > max_num)
+        {
             max_num = arr[i];
         }
-        else if(arr[i] < min_num){
+        if (arr[i] < min_num)
+        {
             min_num = arr[i];
         }
     }
-
-
-    cout <<"Maximum value in array is : " << max_num << endl;
-
-
-    cout <<"Minimum value in array is : " << min_num << endl;
-
-
+    cout << "Maximum value in array is : " << max_num << endl;
+    cout << "Minimum value in array is : " << min_num << endl;
     return 0;
-
-
-
 }
